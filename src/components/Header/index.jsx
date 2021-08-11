1138ac2302d66867ecd3e4508a5f6e0062ff3e96
marville001 <mwangimartin1904@gmail.com>
2021-08-11 14:58:45 +0300

    Added Map

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -3,7 +3,7 @@ import {Autocomplete} from '@react-google-maps/api'
 import {AppBar, Toolbar, Typography, InputBase, Box} from '@material-ui/core'
 import SearchIcon from '@material-ui/icons/Search'
 
-import useStyles from './'
+import useStyles from './styles'
 
 export default () => {
     const classes = useStyles();
--- src/components/Map/index.jsx
+++ src/components/Map/index.jsx
@@ -1,9 +1,33 @@
 import React from 'react'
+import GoogleMapReact from 'google-map-react';
+import { Paper, Typography, useMediaQuery } from '@material-ui/core';
+import LocationOnOutlinedIcon from '@material-ui/icons/LocationOnOutlined';
+import Rating from '@material-ui/lab/Rating';
+
+import mapStyles from "../mapStyle";
+import useStyles from './styles.js';
 
 export default () => {
+    const matches = useMediaQuery('(min-width:600px)');
+    const classes = useStyles();
+
+    const coordinates = {lat: 0, lng: 0}
+
+
     return (
-        <div>
-            <h4>Map</h4>
+        <div className={classes.mapContainer}>
+            <GoogleMapReact
+                bootstrapURLKeys={{key:'AIzaSyCGBulc9eXW2deFLeO-bOUcPzMWZ9o9Slg'}}
+                defaultCenter={coordinates}
+                center={coordinates}
+                defaultZoom={14}
+                margin={[50,50,50,50]}
+                options={''}
+                onChange={''}
+                onChildClick={''}
+            >
+
+            </GoogleMapReact>
         </div>
     )
 }
