82470f2c7bb5f5417f509663f103f299ce419bd4
marville001 <mwangimartin1904@gmail.com>
2021-08-11 16:13:52 +0300

    Added Places Card

--- src/components/PlaceDetails/index.jsx
+++ src/components/PlaceDetails/index.jsx
@@ -4,13 +4,22 @@ import LocationOnIcon from '@material-ui/icons/LocationOn';
 import PhoneIcon from '@material-ui/icons/Phone';
 import Rating from '@material-ui/lab/Rating';
 
-import useStyles from './styles.js';
+import useStyles from './styles';
 
 export default ({place}) => {
-    // const classes = useStyles();
+    const classes = useStyles();
+
+    const imgUrl = place.photo? place.photo.images.large.url:'https://www.foodserviceandhospitality.com/wp-content/uploads/2016/09/Restaurant-Placeholder-001.jpg';
     return (
-        <div>
-            <h4>{place.name}</h4>
-        </div>
+        <Card elevation={6}>
+            <CardMedia 
+                style={{height: 350}}
+                image={imgUrl}
+                title={place.name}
+            />
+            <CardContent>
+                <Typography gutterBottom variant="h5">{place.name}</Typography>
+            </CardContent>
+        </Card>
     )
 }
